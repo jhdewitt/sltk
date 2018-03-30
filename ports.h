9d5ffa3273df8e9a87272881f195526fb8aaf142
John De Witt <jhdewitt+github@gmail.com>
2018-03-30 07:04:58 -0700

    update ports.h

--- ports.h
+++ ports.h
@@ -1,6 +1,7 @@
+#define SERVER_CONTROL_ADDRESS "0.0.0.0"
 #define SERVER_DISPLAY_ADDRESS "0.0.0.0"
-#define SERVER_DISPLAY_PORT "3210" //4001 // from client for displaying patterns
-#define CLIENT_DISPLAY_PORT "4011" // to client
+#define SERVER_CAMERA_ADDRESS "0.0.0.0"
+#define SERVER_SERIAL_ADDRESS "0.0.0.0"
 
 #define SERVER_CAMERA_PORT  "4002" // from client for image capture
 #define CLIENT_CAMERA_PORT  "4012"
